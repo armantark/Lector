fa9788504762211d46c66c09734a09e444f231ea
armantark <armantarkhanian@berkeley.edu>
2021-02-24 19:55:37 -0800

    database fixed, changed to postgresql, also config.json no longer necessary

--- helpers/logger.py
+++ helpers/logger.py
@@ -3,10 +3,8 @@ import json
 
 def log(message):
 	log_webhook = os.environ['log_webhook']
-    # with open('config.json','r') as f:
-    #     log_webhook = json.load(f)['log_webhook']
 
-    if log_webhook != '':
+	if log_webhook != '':
         requests.post(log_webhook, json={
             "content":message
         })
