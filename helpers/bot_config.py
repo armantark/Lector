93db6c4af5113971349e642cc6cf609bf83e6917
armantark <armantarkhanian@berkeley.edu>
2021-02-24 19:37:26 -0800

    database fixed, changed to postgresql, also config.json no longer necessary

--- helpers/bot_config.py
+++ helpers/bot_config.py
@@ -9,20 +9,20 @@ class Config:
     prefix = os.environ['prefix']
     token = os.environ['token']
 
-    try:
-        with open(filename, 'r') as f:
-            data = json.load(f)
+    # try:
+    #     with open(filename, 'r') as f:
+    #         data = json.load(f)
         
-        del data
+    #     del data
 
-    except FileNotFoundError:
-        print(f'\'{filename}\' does not exist.')
-        sys.exit()
+    # except FileNotFoundError:
+    #     print(f'\'{filename}\' does not exist.')
+    #     sys.exit()
 
-    except json.decoder.JSONDecodeError:
-        print(f'\'{filename}\' is malformed.')
-        sys.exit()
+    # except json.decoder.JSONDecodeError:
+    #     print(f'\'{filename}\' is malformed.')
+    #     sys.exit()
 
-    except KeyError:
-        print(f'\'{filename}\' is missing information.')
-        sys.exit()
+    # except KeyError:
+    #     print(f'\'{filename}\' is missing information.')
+    #     sys.exit()
