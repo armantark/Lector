08ecc5240d7d9fe5dc54b10a9d77d5df1b838541
armantark <armantarkhanian@berkeley.edu>
2021-04-04 00:56:52 -0700

    fixed Armenian lectionary not working with multiple h4 tags

--- lectionary/armenian.py
+++ lectionary/armenian.py
@@ -63,8 +63,12 @@ class ArmenianLectionary:
 
         self.title    = soup.select('h2')[1].text
         self.subtitle = date_expand.auto_expand(self.today, self.title)
+        readings = []
 
-        readings = soup.select_one('h4[style]').text
+        readings_raw_select = soup.select('h4[style]')
+
+        for reading in readings_raw_select:
+            readings += reading.text
 
         substitutions = {'III ':'3 ','II ':'2 ','I ':'1 ','Azariah':'Prayer of Azariah'}
         for original in substitutions.keys():
