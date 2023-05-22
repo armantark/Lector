7710629bb2e23b9b39e1864b3f20c1f4e3087428
Arman Tarkhanian <atarkhanian@tynker.com>
2023-05-21 22:12:24 -0700

    refactored date_expand.py

--- helpers/date_expand.py
+++ helpers/date_expand.py
@@ -1,27 +1,22 @@
-import datetime
+from num2words import num2words
 
-def ordinal(day):
-    '''
-    Get the ordinal indicator for a day of the month
-    '''
-    indicators = {1:'st',2:'nd',3:'rd',21:'st',22:'nd',23:'rd',31:'st'}
-    if day in indicators: return indicators[day]
-    else:                 return 'th'
 
 def expand(dateobject):
-    '''Given a date object, returns a specially formatted string representing it w/ the weekday'''
-    return dateobject.strftime(f'%A, %B {dateobject.day}{ordinal(dateobject.day)}, %Y')
+    """Given a date object, returns a specially formatted string representing it w/ the weekday"""
+    return dateobject.strftime(f'%A, %B {dateobject.day}{num2words(dateobject.day, ordinal=True)}, %Y')
+
 
 def expand_no_weekday(dateobject):
-    '''Given a date object, returns a specially formatted string representing it w/o the weekday'''
-    return dateobject.strftime(f'%B {dateobject.day}{ordinal(dateobject.day)}, %Y')
+    """Given a date object, returns a specially formatted string representing it w/o the weekday"""
+    return dateobject.strftime(f'%B {dateobject.day}{num2words(dateobject.day, ordinal=True)}, %Y')
+
 
 def auto_expand(dateobject, text):
-    '''
+    """
     If some text does not contain the weekday, generate a datestamp
     that *does* contain the weekday. If the text contains it, the
     datestamp should not contain it
-    '''
+    """
     if dateobject.strftime('%A') not in text:
         return expand(dateobject)
     else:
