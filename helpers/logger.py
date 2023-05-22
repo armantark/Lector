200f15b586ae74fdd22990f7aa1a1ea16133bb3a
Arman Tarkhanian <atarkhanian@tynker.com>
2023-05-21 22:13:14 -0700

    refactored logger.py

--- helpers/logger.py
+++ helpers/logger.py
@@ -1,11 +1,12 @@
-import requests
-import json
 import os
+
+import requests
+
 
 def log(message):
     log_webhook = os.environ['log_webhook']
 
     if log_webhook != '':
         requests.post(log_webhook, json={
-            "content":message
+            "content": message
         })
