e5ecb1a5000bc2be7fb03657d10de8d9bf2d98df
armantark <armantarkhanian@berkeley.edu>
2025-05-13 13:41:27 -0700

    fixed trailing whitespace on command issue

--- helpers/bot_config.py
+++ helpers/bot_config.py
@@ -8,7 +8,7 @@ load_dotenv()  # take environment variables from .env.
 class Config:
     def __init__(self):
         self.prefix = os.getenv('prefix')
-        if prefix and not prefix.endswith(' '):
-            prefix += ' '
+        if self.prefix and not self.prefix.endswith(' '):
+            self.prefix += ' '
         self.token = os.getenv('token')
         self.log_webhook = os.getenv('log_webhook')
